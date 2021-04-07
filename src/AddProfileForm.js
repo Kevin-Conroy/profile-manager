2bf738aa289d612c019e466a1507befdaa002e9e
Kevin Conroy <Kcdrumming@gmail.com>
2021-04-07 15:34:06 -0700

    4-7 working commit

--- src/AddProfileForm.js
+++ src/AddProfileForm.js
@@ -3,59 +3,63 @@ import React, { Component } from "react";
 import ProfileData from "./ProfileData";
 import UserProfile from "./UserProfile";
 import Header from "./Header";
+import App from "./App";
 
 class AddProfileForm extends Component {
   constructor(props) {
     super(props);
+    this.addProfile = this.addProfile.bind(this);
     this.state = {
-      
-        firstName: "",
-        lastName: "",
-        role: "",
-        id: ""
-      
+      firstName: "",
+      lastName: "",
+      role: "",
+      id: "",
     };
   }
 
+  addProfile = () => {
+    this.props.addProfile();
+  };
+
   handleSubmit = (event) => {
     event.preventDefault();
+    this.props.addProfile(this.state);
   };
 
-  handleFirstName = (event) => {
-    event.preventDefault();
-    this.setState({firstName: event.target.value})
-  };
-
-  handleLastName = (event) => {
-    event.preventDefault();
-    this.setState({lastName: event.target.value})
-  };
-
-  handleRole = (event) => {
-    event.preventDefault();
-    this.setState({role: event.target.value})
-  };
-
-  handleId = (event) => {
-    event.preventDefault();
-    this.setState({id: event.target.value})
+  handleInput = (field) => (event) => {
+    this.setState({ [field]: event.target.value });
   };
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <input
+          id="firstName"
           type="text"
           placeholder="First Name"
-          onChange={this.handleFirstName}
+          onChange={this.handleInput("firstName")}
         />
         <input
+          id="lastName"
           type="text"
           placeholder="Last Name"
-          onChange={this.handleLastName}
+          onChange={this.handleInput("lastName")}
         />
-        <input type="text" placeholder="Role" onChange={this.handleRole} />
-        <input type="text" placeholder="Id #" onChange={this.handleId} />
+        <input
+          id="role"
+          type="text"
+          placeholder="Role"
+          onChange={this.handleInput("role")}
+        />
+        <input
+          id="id"
+          type="text"
+          placeholder="Id #"
+          onChange={this.handleInput("id")}
+        />
+        <button type="submit" onSubmit={this.handleSubmit}>
+          Add Profile
+        </button>
       </form>
     );
   }
--- src/App.js
+++ src/App.js
@@ -8,17 +8,29 @@ import AddProfileForm from "./AddProfileForm";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.addProfile = this.addProfile.bind(this);
     this.state = {
       profileList: [],
     };
   }
 
+  addProfile = (profile) => {
+    this.setState({ profileList: [...this.state.profileList, profile] });
+  };
+
   render() {
     return (
       <div>
         <Header />
-        <AddProfileForm />
-        <ProfileData />
+        <AddProfileForm addProfile={this.addProfile} />
+        {this.state.profileList.map((profile) => (
+          <UserProfile
+            firstName={profile.firstName}
+            lastName={profile.lastName}
+            role={profile.role}
+            id={profile.id}
+          />
+        ))}
       </div>
     );
   }
